ab671bcb5175147ad1a7fff45cfb117ab4601a5f
HENG-Y431 <natsume0431@gmail.com>
2026-02-21 05:40:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,22 @@
-/* config.js - 文明法典：調整數值請找我 所有數值都在這裡改 */
+/* config.js - 文明法典：V9.6 校正版 */
 const CONFIG = {
-    GAME_SPEED: 250,           // 遊戲速度
-    SOCIAL_RANGE: 130,         // 社交範圍
-    INITIAL_GRACE: 200,        // 初始神恩
-    YEARLY_GRACE: 5,           // 每年自動增加
-    
-    // 神選門檻
-    HERO_TOTAL_MIN: 58,        // 總合標準
-    HERO_PEAK_MIN: 17,         // 單項巔峰
-    HERO_PEAK_COUNT: 2,        // 需達兩項
-    
-    // 神蹟消耗
-    COST_FOOD: 100,            // 聖餐
-    COST_ENERGY: 50,           // 治癒
-    COST_LOVE: 200,            // 紅線
-
+    GAME_SPEED: 250,           
+    SOCIAL_RANGE: 130,         
+    INITIAL_GRACE: 200,        
+    YEARLY_GRACE: 5,           
+    HERO_TOTAL_MIN: 58,        
+    HERO_PEAK_MIN: 17,         
+    HERO_PEAK_COUNT: 2,        
+    COST_FOOD: 100,            
+    COST_ENERGY: 50,           
+    COST_LOVE: 200,            
     MINS_IN_YEAR: 60 * 24 * 30 * 12,
-    MINS_IN_MONTH: 60 * 24 * 30, // 一個月
-    MINS_IN_DAY: 60 * 24,        // 一天
-    
-    FOOTER_HEIGHT: 115         // 下方欄位高度
+    MINS_IN_MONTH: 60 * 24 * 30, 
+    MINS_IN_DAY: 60 * 24,        
+    FOOTER_HEIGHT: 115         
 };
 
-
-// 全域數據存放
+// --- 全域變數 (全文明僅此處聲明，禁止在 script.js 重複寫) ---
 let villagers = [], environment = [], selectedId = null, matchId = null;
 let totalMinutes = 0, genCounters = {}, deathCount = 0, currentTab = 'All';
 let plagueZone = null, gracePoints = CONFIG.INITIAL_GRACE;
