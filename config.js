5f6a18c60ae9b5a8ad55d054e408d8ad8da54b4f
HENG-Y431 <natsume0431@gmail.com>
2026-02-21 04:58:03 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+/* config.js - 文明法典：調整數值請找我 所有數值都在這裡改 */
+const CONFIG = {
+    GAME_SPEED: 250,           // 遊戲速度
+    SOCIAL_RANGE: 130,         // 社交範圍
+    INITIAL_GRACE: 200,        // 初始神恩
+    YEARLY_GRACE: 5,           // 每年自動增加
+    
+    // 神選門檻
+    HERO_TOTAL_MIN: 58,        // 總合標準
+    HERO_PEAK_MIN: 17,         // 單項巔峰
+    HERO_PEAK_COUNT: 2,        // 需達兩項
+    
+    // 神蹟消耗
+    COST_FOOD: 100,            // 聖餐
+    COST_ENERGY: 50,           // 治癒
+    COST_LOVE: 200,            // 紅線
+    
+    MINS_IN_YEAR: 60 * 24 * 30 * 12,
+    FOOTER_HEIGHT: 115         // 下方欄位高度
+};
